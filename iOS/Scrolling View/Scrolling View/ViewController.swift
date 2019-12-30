174e0885d4d94f48a91a9a4cd44c264b532ffdbf
Archie Disono <disonoarchie@gmail.com>
2019-12-30 16:19:44 +0800

    Initialize scrolling view

---
+++ iOS/Scrolling View/Scrolling View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scrolling View
+//
+//  Created by Archie on 12/28/19.
+//  Copyright © 2019 Archie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS/Scrolling View/Scrolling ViewTests/Scrolling_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Scrolling_ViewTests.swift
+//  Scrolling ViewTests
+//
+//  Created by Archie on 12/28/19.
+//  Copyright © 2019 Archie. All rights reserved.
+//
+
+import XCTest
+@testable import Scrolling_View
+
+class Scrolling_ViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
